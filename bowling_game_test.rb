6b1e70d1e66cf4a9c631f6d3db3a5d8eaaa65bb9
maitamin <3617510+maitamin@users.noreply.github.com>
2024-12-29 16:38:40 +0900

    P71 スペアの実装

--- bowling_game_test.rb
+++ bowling_game_test.rb
@@ -16,8 +16,27 @@ class BowlingGameTest < Minitest::Unit::TestCase
     assert_equal 20, @game.score
   end
 
+  def test_スペアを取った場合
+    @game.record_shot(3)
+    @game.record_shot(7) # 10 + 4 = 14
+    @game.record_shot(4)
+    record_many_shots(17, 0)
+    assert_equal 18, @game.score
+  end
+
+  def test_直前の投球との合計が10ピンでもフレーム違いはスペアではない
+    @game.record_shot(2)
+    @game.record_shot(5)
+    @game.record_shot(5) # 前の投球との合計が10でもフレーム違いなのでスペアではない
+    @game.record_shot(1)
+    record_many_shots(16, 0)
+
+    assert_equal 13, @game.score
+  end
+
+
   private
-  
+
   def record_many_shots(count, pins)
     count.times do
       @game.record_shot(pins)
